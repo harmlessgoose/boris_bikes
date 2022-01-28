5d21820247580a638e2dd8430ff279e3f879232f
Peter O <harmless_goose@icloud.com>
2022-01-28 09:20:53 +0000

    Cleaned up code

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,16 +3,16 @@ require 'docking_station'
 describe DockingStation do
   
   it {is_expected.to respond_to :release_bike}
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bikes_in_station) } 
   
   it "releases working bikes" do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.release_bike).to be_working
   end
-
-  it { is_expected.to respond_to(:dock).with(1).argument }
-
-  it { is_expected.to respond_to(:bikes_in_station) } 
 
   it "docks something" do
     bike = Bike.new
